db574b1fc28d68456297cab232e56791b458130b
Manthanchavda27 <manthnachavda2707@gmail.com>
2025-08-27 19:08:52 +0530

    Message

---
+++ Samplefolder/restaurant_menu.js
@@ -0,0 +1,21 @@
+const breakfastMenu = ['Poha-12','Thepla-20','Tea-10'];
+const maincourseMenu = ['Gujrati thali-100','Punjabi thali-200','dal-50','roti-15'];
+const dessertMenu = ['Ice cream','gulab jamun','cake','Fruit Salad'];
+
+const breakfastMenuItemsHTml = breakfastMenu.map((item,index)=>
+    `<p>Item ${index+1}:${item}<p>`).join('');
+    document.getElementById('breakfastMenuItems').innerHTML=breakfastMenuItemsHTml;
+
+
+
+let maincourse=" ";
+    maincourseMenu.forEach((item,i)=>{
+    maincourse+=`<p>Item ${i+1}:${item}<p>`;});
+    document.getElementById('maincourseMenuItems').innerHTML=maincourse;
+
+let dessert=" ";
+    for(let i=0;i<dessertMenu.length;i++){
+        dessert+=`<p>Item ${i+1}:${dessertMenu[i]}</p>`;
+    }
+    document.getElementById('dessertMenuItems').innerHTML=dessert;
+    
